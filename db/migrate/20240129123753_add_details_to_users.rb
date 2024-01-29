87d687dde00fa4de4eb9fb6f5685e03baa55d307
Tony Masek <tm@tony-masek.com>
2024-01-29 13:38:10 +0100

    Update user model

---
+++ db/migrate/20240129123753_add_details_to_users.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :company_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :company_address, :string
+    add_column :users, :ico, :string
+    add_column :users, :dic, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_123634) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_123753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_123634) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company_name"
+    t.string "phone_number"
+    t.string "company_address"
+    t.string "ico"
+    t.string "dic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
