710b7aafb8b743ccb34c16f6565f2ad6b7916bd2
Tony Masek <tm@tony-masek.com>
2024-02-25 21:47:08 +0100

    Create the environment for production
    Made the env production ready meaning that, it diffs between live stripe keys and test stripe keys.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,11 @@
-Stripe.api_key = ENV.fetch('STRIPE_TEST_KEY')
+if Rails.env.production?
+  # Use live keys in production
+  Stripe.api_key = ENV.fetch('STRIPE_LIVE_SECRET_KEY')
+  Rails.application.config.x.stripe.publishable_key = ENV.fetch('STRIPE_LIVE_PUBLISHABLE_KEY')
+  Rails.application.config.x.stripe.price_id = ENV.fetch('STRIPE_LIVE_PRICE_ID')
+else
+  # Use test keys in development and other environments
+  Stripe.api_key = ENV.fetch('STRIPE_TEST_SECRET_KEY')
+  Rails.application.config.x.stripe.publishable_key = ENV.fetch('STRIPE_TEST_PUBLISHABLE_KEY')
+  Rails.application.config.x.stripe.price_id = ENV.fetch('STRIPE_TEST_PRICE_ID')
+end
