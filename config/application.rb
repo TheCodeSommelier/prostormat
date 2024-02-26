b48a0a2067f60a5d38bc7f4de6129a638f7410f8
Tony Masek <tm@tony-masek.com>
2024-02-26 18:37:22 +0100

    Add config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module SpaceMi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.service = :cloudinary
+
+    config.assets.enabled = true
+
   end
 end
