8b7ad4c06c678d465db498551ddb3287d867995f
Tony Masek <tm@tony-masek.com>
2024-02-27 16:45:58 +0100

    Configure sidekiq and postmark for sending emails and processing background jobs
    Postmark is an emailing service used to send and recieve emails. And sidekiq is used for background jobs processing

---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,18 @@
+class TestMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.test_mailer.hello.subject
+  #
+  def hello
+    @greeting = "Hi"
+
+    mail(
+      subject: 'Hello from Postmark',
+      to: 'poptavka@prostormat.cz',
+      from: 'poptavka@prostormat.cz',
+      track_opens: 'true',
+      message_stream: 'broadcast'
+    )
+  end
+end
