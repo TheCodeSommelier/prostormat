06bbc037e4581ad5b16c7b022d20a26570569def
Tony Masek <tm@tony-masek.com>
2024-02-27 12:05:38 +0100

    Correct the path to application.css and .scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../builds
-//= link application.scss
-//= link application.css
+//= link ../styleheets/application.scss
+//= link ../stylesheets/application.css
