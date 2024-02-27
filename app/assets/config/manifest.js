435db3500db7c99bcf5ce84a860775a17be6501c
Tony Masek <tm@tony-masek.com>
2024-02-27 12:08:44 +0100

    Change the path to application.scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../builds
-//= link ../../styleheets/application.scss
-//= link ../../stylesheets/application.css
+//= link ../styleheets/application.scss
+//= link ../stylesheets/application.css
