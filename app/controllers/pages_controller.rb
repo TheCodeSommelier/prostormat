5a32706c8eaa94237a468558648ed9ee6efa0f08
Tony Masek <tm@tony-masek.com>
2024-03-23 19:54:11 +0100

    Trying manually closing connections to DB

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,9 @@ class PagesController < ApplicationController
   # The landing_page action renders the application's landing page, which is used to explain
   # how it works, sample places/venues and a footer
   def landing_page
-    @places = Place.all.sample(2)
+    ActiveRecord::Base.connection_pool.with_connection do
+      @places = Place.all.sample(2)
+    end
   end
 
   def about_us; end
