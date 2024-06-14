54c32d5927554cdc6ae6a3ad88c0bebf95958648
Tony Masek <tm@tony-masek.com>
2024-06-14 14:27:59 +0200

    Add char counter

---
+++ app/javascript/controllers/char_counter_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="char-counter"
+export default class extends Controller {
+  static targets = ["textarea", "counter"];
+  static values = { max: Number };
+
+  connect() {
+    this.updateCounter();
+  }
+
+  #updateCounter() {
+    this.textareaTargets.forEach((textarea, index) => {
+      const counter = this.counterTargets[index];
+      const maxChars = this.maxValue;
+      const charCount = textarea.value.length;
+      counter.textContent = `${charCount}/${maxChars}`;
+      this.#counter(textarea, charCount, maxChars, counter)
+
+
+    });
+  }
+
+  count() {
+    this.#updateCounter();
+  }
+
+  #counter(textarea, charCount, maxChars, counter) {
+    if (textarea.id === 'place_long_description') {
+      this.#setStyle(textarea, charCount, maxChars, counter, true)
+    } else {
+      this.#setStyle(textarea, charCount, maxChars, counter, false)
+    }
+  }
+
+  #setStyle(textarea, charCount, maxChars, counter, shouldGoOver) {
+    if (charCount < maxChars && shouldGoOver) {
+      textarea.style.border = "red 2px solid";
+      counter.style.color = "red";
+      textarea.value = textarea.value.substring(0, maxChars);
+      counter.textContent = `Musí být přes ${maxChars} znaků máte ${charCount}`;
+    } else if (charCount > maxChars && !shouldGoOver) {
+      textarea.style.border = "red 2px solid";
+      counter.style.color = "red";
+      textarea.value = textarea.value.substring(0, maxChars);
+      counter.textContent = `${maxChars}/${maxChars}`;
+    } else {
+      textarea.style.border = "lightgrey 1px solid";
+      counter.style.color = "#888";
+    }
+  }
+}
